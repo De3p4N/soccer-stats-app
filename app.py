2958dcffbda7045a3ca05cd04ed859feca4a143f
De3p4N <deepanchauhan972@gmail.com>
2026-06-22 16:30:12 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,137 @@
+# The app.py uses Flask, the python web framework, to connect the footballClient.py
+# file to the html files. Running this will run the app 
+
+from flask import Flask, render_template
+from flask_caching import Cache
+from footballClient import(get_league_standings, get_live_matches, 
+                           get_top_scorers, get_upcoming_matches)
+
+# This part of the code configures and initializes the caching method
+config ={
+    "DEBUG": True,
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+app = Flask(__name__)
+app.config.from_mapping(config)
+cache = Cache(app)
+
+# These are the supported leagues given by the free version of the API. This dictionary is
+# key:value pairs of {league_code : league_name}
+LEAGUES = {
+    "PL": "Premier League",
+    "PD" : "Primera Division", # La Liga
+    "BL1" : "Bundesliga",
+    "SA" : "Serie A",
+    "FL1" : "Ligue 1",
+    "DED": "Eredivisie",
+    "PPL": "Primeira Liga", # Portuguese League
+    "EC" : "European Championship",  # Euros
+    "ELC" : "Championship", # 2nd tier English football
+    "BSA" : "Campeonato Brasileiro Série A",  # Brazilian League
+    "CL" : "UEFA Champions League",
+    "WC" :  "FIFA World Cup",
+    "CLI" : "Copa Libertadores" # South American club competition
+}
+
+# images of competition logos
+LEAGUE_IMAGES = {
+    "PL" : "images/premierLeague.png",
+    "PD" : "images/laliga.png",
+    "BL1" : "images/Bundesliga.png",
+    "SA" : "images/serieA.png",
+    "FL1" : "images/Ligue_1.png",
+    "DED" : "images/Eredivisie.png",
+    "PPL" : "images/portuguese_league.png",
+    "EC" : "images/euros.png",
+    "ELC" : "images/EFL_Championship_Logo.png",
+    "BSA" : "images/brazil_league.png",
+    "CL" : "images/UEFA_Champions_League.png",
+    "WC" : "images/worldcup.png",
+    "CLI" : "images/Copa_Libertadores_logo.png",
+}
+
+# This app route is for the home page of the site
+@app.route('/')
+@cache.cached(timeout=600) # I cached the homepage for 600 seconds (10 minutes)
+def index():
+    return render_template('index.html',leagues=LEAGUES, league_images=LEAGUE_IMAGES)
+
+# This app route is for the about page of the site
+@app.route('/about')
+@cache.cached(timeout=600) # I cached the homepage for 600 seconds (10 minutes)
+def about():
+    return render_template('about.html',leagues=LEAGUES, league_images=LEAGUE_IMAGES)
+
+
+# The get_live_matches() and get_upcoming_matches() are both smaller functions
+# that are split up to make the matches.html file work and display live and future
+# matches. I split them to cache live responses for 5 minutes, and future matches for 15 minutes
+
+# Live scores cached data
+@cache.memoize(timeout=300)  # 5 minutes
+def cache_live_matches(league_code):
+    try:
+        return get_live_matches(league_code)
+    except Exception as e:
+        print(f"API Error on live matches: {e}")
+        return []
+
+
+# Separate, longer cache for the upcoming matches only
+@cache.memoize(timeout=900) # 15 minutes 
+def cache_upcoming_matches(league_code):
+    try:
+        return get_upcoming_matches(league_code)
+    except Exception as e:
+        print(f"API Error on today's matches: {e}")
+        return []
+
+
+# The route calls the two cached helper functions.
+@app.route('/matches/<league_code>')
+def upcoming_matches(league_code):
+    upcoming_matches_data = cache_upcoming_matches(league_code)
+    live_matches = cache_live_matches(league_code)
+
+    league_name = LEAGUES.get(league_code, league_code)
+    return render_template('matches.html',
+                            leagues=LEAGUES,
+                            upcoming_matches=upcoming_matches_data,
+                            live_matches=live_matches,
+                            league_images=LEAGUE_IMAGES,
+                            league_name=league_name)
+
+# This app route is for league standings and top scorers
+@app.route('/standings/<league_code>')
+@cache.memoize(timeout=900) # Used cache.memoize() for dynamic URLs, cached for 15 minutes
+def standings(league_code):
+    standings_data = []
+    scorer_list = []
+    scorers_data = []
+    try:
+        standings_data = get_league_standings(league_code)
+    except Exception as e:
+        print(f"API Error on standings: {e}")
+        
+    try:
+        scorers_data = get_top_scorers(league_code)
+        
+        if isinstance(scorers_data, dict):
+            scorer_list = scorers_data.get("scorers", [])
+                  
+    except Exception as e:
+        print(f"API Error on scorers: {e}")
+
+    league_name = LEAGUES.get(league_code, league_code)
+
+    return render_template('leaguedata.html',
+                           standings=standings_data,
+                           scorers=scorer_list,
+                           current_league=league_code,
+                           leagues=LEAGUES,
+                           league_name=league_name,
+                           league_images=LEAGUE_IMAGES)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5555,debug=True)
---
+++ footballClient.py
@@ -0,0 +1,127 @@
+# The footballClient.py accesses various data from the API. This file contains the functions 
+# used to access each type of data. 
+
+import requests 
+import os # needed to use data from .env file
+from dotenv import load_dotenv # this imports and uses the API key in the .env file
+from datetime import date, datetime,timedelta # used in get_todays_matches()
+from zoneinfo import ZoneInfo # used to convert timezones to Central Time.
+
+# This is used so people don't use your API key
+load_dotenv()
+
+base_url = "https://api.football-data.org/v4"
+api_key = os.getenv("Football_API_KEY") # used the variable in the .env file
+
+header = {
+    "X-Auth-Token": api_key
+}
+
+# This function returns competitions
+def get_competitions():
+    response = requests.get(f"{base_url}/competitions", headers=header)
+    if response.status_code == 200: # status code 200 means the data was retrieved successfully
+        data = response.json()
+        competitions = data["competitions"]
+        return competitions
+    else:
+        print("Error fetching competitions:", response.status_code) 
+        return []
+
+# This returns league standings
+def get_league_standings(league_code):
+    response = requests.get(f"{base_url}/competitions/{league_code}/standings", headers=header)
+    if response.status_code == 200:
+        data = response.json()
+        return data.get("standings", [])
+    else:
+        print("Error fetching standings:", response.status_code)
+        return []     
+
+# returns all current live matches
+def get_live_matches(league_code=None):
+    params = {
+        "status": "LIVE",
+    }
+
+    response = requests.get(
+        f"{base_url}/matches",
+        headers=header,
+        params=params
+    )
+
+    if response.status_code == 200:
+        data = response.json()
+        matches = data["matches"]
+        # This filters all live matches by competition and then returns the filtered matches 
+        filtered_matches = []
+
+        for match in matches:
+            if match["competition"]["code"] == league_code:
+                filtered_matches.append(match)
+
+        matches = filtered_matches
+        return matches
+
+    else:
+        print("Error fetching Live Matches", response.status_code)
+        return []
+
+# returns matches scheduled for the date the program is accessed
+def get_upcoming_matches(league_code):
+
+    today = date.today()
+
+    params = {
+        "dateFrom": today.strftime("%Y-%m-%d"),
+        "dateTo": (today + timedelta(days=30)).strftime("%Y-%m-%d"),
+        "status": "SCHEDULED",
+    }
+
+    response = requests.get(
+        f"{base_url}/competitions/{league_code}/matches",
+        headers=header,
+        params=params
+    )
+
+    if response.status_code == 200:
+        data = response.json()
+        matches = data["matches"]   
+
+        # This loop returns all match times in Central Time. 
+        central = ZoneInfo("America/Chicago")
+
+        today_ct = datetime.now(central).date()
+        tomorrow_ct = today_ct + timedelta(days=1)
+
+        for match in matches:
+            utc_str = match.get("utcDate")
+
+            if utc_str:
+                dt_utc = datetime.fromisoformat(utc_str.replace("Z", "+00:00"))
+                dt_local = dt_utc.astimezone(central)
+
+                match_date_ct = dt_local.date()
+                if match_date_ct == today_ct:
+                    match["match_date"] = "Today"
+                elif match_date_ct == tomorrow_ct:
+                    match["match_date"] = "Tomorrow"
+                else:
+                    match["match_date"] = dt_local.strftime("%b %d, %Y")
+
+                match["kickoff_time"] = dt_local.strftime("%I:%M %p")
+                match["timezone_label"] = "CT"
+                
+        return matches
+    else:
+        print("Error fetching today's Matches:", response.status_code)
+        return []
+    
+# returns the top goalscoring players for their respective leagues
+def get_top_scorers(league_code):
+    response = requests.get(f"{base_url}/competitions/{league_code}/scorers", headers=header, params={"limit":10}) # API only gives top 10
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Error fetching top goalscorers", response.status_code)
+        return []
